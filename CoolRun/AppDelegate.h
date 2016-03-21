a6899b42cb7b19de1a2e84bc90172ce0d0bc1424
caroline <caroline.hjq@gmail.com>
2016-03-22 00:42:12 +0800

    update xmpp by self. create property xmppstream

--- CoolRun/AppDelegate.h
+++ CoolRun/AppDelegate.h
@@ -7,11 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "XMPPFramework.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
+@property (strong,nonatomic) XMPPStream *xmppStream;
 @end
 
